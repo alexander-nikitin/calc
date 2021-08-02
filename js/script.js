cffeccc0f2ce944ea328d65d059429b4fc31dca5
Александр Никитин <walter@MacBook-Air-Aleksandr.local>
2021-08-02 23:05:34 +0600

    done: lesson03 hard task

--- js/script.js
+++ js/script.js
@@ -1,72 +1,36 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
-let expences1;
-let expences2;
-let expences3;
-let expences4;
-let amount1;
-let amount2;
-let amount3;
-let amount4;
+let lang;
+let week = [];
+let namePerson;
 
-money = 100000;
-income = 'фриланс';
-addExpenses = 'Подписки, Питомец, Подарки, Развлечения, Хотелки';
-deposit = true;
-mission = 1000000;
-period = 12
+lang = 'ru';
 
-console.log(typeof(money));
-console.log(typeof(income));
-console.log(typeof(deposit));
-
-console.log(addExpenses.length);
-
-console.log('Период равен ' + period + ' месяцев');
-console.log('Цель заработать ' + mission + ' рублей');
-
-addExpenses = addExpenses.toLowerCase();
-console.log(addExpenses.split(', '));
-
-let budgetDay = 100000/30
-console.log(budgetDay);
-
-money = prompt('Ваш месячный доход?');
-console.log('money: ' + money);
-
-addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
-console.log('addExpenses: ' + addExpenses);
-
-deposit = confirm('Есть ли у вас депозит в банке?');
-console.log('deposit: ' + deposit);
-
-expences1 = prompt('Введите обязательную статью расходов.');
-amount1 = prompt('Во сколько это обойдется?');
-expences2 = prompt('Введите обязательную статью расходов.');
-amount2 = prompt('Во сколько это обойдется?');
-expences3 = prompt('Введите обязательную статью расходов.');
-amount3 = prompt('Во сколько это обойдется?');
-expences4 = prompt('Введите обязательную статью расходов.');
-amount4 = prompt('Во сколько это обойдется?');
-
-let budgetMonth = +money - (+amount1 + +amount2 + +amount3 + +amount4);
-console.log('Месячный бюджет равен: ' + budgetMonth);
-
-let missionTime = Math.ceil(mission / budgetMonth);
-console.log('Цель будет достигнута через ' + missionTime + ' месяцев.');
-
-budgetDay = budgetMonth / 30;
-
-if ( budgetDay >= 1200 ) {
-    console.log('У вас высокий уровень дохода');
-} else if ( budgetDay >=600) {
-    console.log('У вас средний уровень дохода');
-} else if ( budgetDay < 600) {
-    console.log('К сожалению у вас уровень дохода ниже среднего');
-} else {
-    console.log('Что-то пошло не так');
+week = {
+    'ru': ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'],
+    'en': ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
 }
+
+if ( lang == 'ru' ) {
+    console.log(week['ru']);
+} else if ( lang == 'en' ) {
+    console.log(week['en']);
+}
+
+console.log(week);
+
+switch (lang) {
+    case 'ru':
+        console.log(week['ru']);
+        break;
+    case 'en':
+        console.log(week['en']);
+}
+
+console.log(week);
+
+console.log(week[lang]);
+
+namePerson = 'Артем';
+
+namePerson === 'Артем' ? console.log('Директор') :
+    namePerson === 'Максим' ? console.log('Преподаватель') :
+        console.log('Студент');
